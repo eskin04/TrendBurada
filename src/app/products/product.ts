34ea1960aba3cff48ee7f22f6439d520f66f3a2d
eskin04 <eskin.okan@gmail.com>
2021-10-17 14:51:06 +0300

    Home componentinin oluşturulması

---
+++ src/app/products/product.ts
@@ -0,0 +1,8 @@
+export class Product{
+    id?: number;
+    name?:string;
+    price?:number;
+    stars?:string;
+    category?:string;
+    image?:string;
+}
---
+++ src/app/products/productList.ts
@@ -0,0 +1,27 @@
+import { Product } from "./product";
+
+export class ProductList{
+    productList:Product[]=[
+        {id:1,name:'Dell',price:3000,stars:'4',category:'Bilgisayar',image:'1.jpg'},
+        {id:2,name:'Asus',price:5000,stars:'3',category:'Bilgisayar',image:'2.jpg'},
+        {id:3,name:'Xiaomi Redmi 5',price:1000,stars:'3',category:'Telefon',image:'3.jpg'},
+        {id:4,name:'Apple Watch',price:3500,stars:'5',category:'Aksesuar',image:'4.jpg'},
+        {id:5,name:'Tişört',price:30,stars:'4',category:'Giyim',image:'5.jpg'},
+        {id:6,name:'Deodorant',price:55,stars:'2',category:'Kozmetik',image:'6.jpg'},
+        {id:7,name:'Skechers',price:500,stars:'4',category:'Ayakkabı',image:'7.jpg'},
+        {id:8,name:'Lenovo',price:3500,stars:'1',category:'Bilgisayar',image:'8.jpg'},
+        {id:9,name:'Macbook',price:8000,stars:'4',category:'Bilgisayar',image:'9.jpg'},
+        {id:10,name:'Samsung s6',price:2000,stars:'2',category:'Telefon',image:'10.jpg'},
+        {id:11,name:'Gümüş Kolye',price:100,stars:'3',category:'Aksesuar',image:'11.jpg'},
+        {id:12,name:'Jean',price:100,stars:'5',category:'Giyim',image:'12.jpg'},
+        {id:13,name:'Parfüm',price:120,stars:'4',category:'Kozmetik',image:'13.jpg'},
+        {id:14,name:'Adidas',price:300,stars:'3',category:'Ayakkabı',image:'14.jpg'},
+        {id:15,name:'Puma',price:200,stars:'4',category:'Ayakkabı',image:'15.jpg'},
+        {id:16,name:'Iphone x',price:10000,stars:'4',category:'Telefon',image:'16.jpg'},
+        {id:17,name:'Huwaie P20',price:3000,stars:'5',category:'Telefon',image:'17.jpg'},
+        {id:18,name:'Deri Cüzdan',price:100,stars:'3',category:'Aksesuar',image:'18.jpg'},
+        {id:19,name:'Mont',price:220,stars:'4',category:'Giyim',image:'19.jpg'},
+        {id:20,name:'Makyaj Seti',price:120,stars:'1',category:'Kozmetik',image:'20.jpg'},
+        {id:21,name:'Bot',price:250,stars:'5',category:'Ayakkabı',image:'21.jpg'}
+    ]
+}
